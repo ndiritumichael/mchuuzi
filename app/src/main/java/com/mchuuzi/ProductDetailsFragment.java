8155a1395b9f3da0ead73fc9a6a2736ed5cf5759
ndiritumichael <ndiritumichael5@gmail.com>
2020-05-09 23:08:53 +0300

    worked on the products details activity.

---
+++ app/src/main/java/com/mchuuzi/ProductDetailsFragment.java
@@ -0,0 +1,39 @@
+package com.mchuuzi;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.mchuuzi.models.ProductsModel;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link ProductDetailsFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ProductDetailsFragment extends Fragment {
+    public ProductDetailsFragment() {
+        // Required empty public constructor
+    }
+
+    public static ProductDetailsFragment newInstance(ProductsModel product) {
+
+        Bundle args = new Bundle();
+
+        ProductDetailsFragment fragment = new ProductDetailsFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_product_details, container, false);
+    }
+}
