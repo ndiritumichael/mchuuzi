73e1ed6fd431b12e3b01be830be66a4f99d3a4b8
AllanMwirigi <allo.mwirigi@gmail.com>
2020-05-09 23:23:54 +0300

    added .idea to gradle

---
+++ app/src/main/java/com/mchuuzi/PaymentDialog.java
@@ -0,0 +1,28 @@
+package com.mchuuzi;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+public class PaymentDialog extends DialogFragment {
+
+    Button btnOk;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.frag_payment_dialog, container, false);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
---
+++ app/src/main/java/com/mchuuzi/TransactionReceiver.java
@@ -0,0 +1,46 @@
+package com.mchuuzi;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import java.util.HashMap;
+
+public class TransactionReceiver extends BroadcastReceiver {
+    public TransactionReceiver() { }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String uuid = intent.getStringExtra("uuid");
+        String confirmationCode = null, amount = null, recipientName = null, recipientPhone = null, date = null, time=null, balance=null, fee=null;
+        if (intent.hasExtra("parsed_variables")) {
+            HashMap<String, String> parsed_variables = (HashMap<String, String>) intent.getSerializableExtra("parsed_variables");
+            if (parsed_variables.containsKey("confirmCode"))
+                confirmationCode = parsed_variables.get("confirmCode");
+            if (parsed_variables.containsKey("balance"))
+                balance = parsed_variables.get("balance");
+            if (parsed_variables.containsKey("amount"))
+                amount = parsed_variables.get("amount");
+            if (parsed_variables.containsKey("recipientName"))
+                recipientName = parsed_variables.get("recipientName");
+            if (parsed_variables.containsKey("recipientPhone"))
+                recipientPhone = parsed_variables.get("recipientPhone");
+            if (parsed_variables.containsKey("date"))
+                date = parsed_variables.get("date");
+            if (parsed_variables.containsKey("time"))
+                time = parsed_variables.get("time");
+            if (parsed_variables.containsKey("fee"))
+                fee = parsed_variables.get("fee");
+
+            String [] arr = {confirmationCode, amount, recipientName, recipientPhone, date, time, balance, fee};
+            Intent toActivityIntent = new Intent("HoverReceiver");
+            toActivityIntent.putExtra("message", arr);
+            context.sendBroadcast(toActivityIntent);
+            Log.e("Hover Receiver", "data: " + confirmationCode+amount+recipientName+recipientPhone+date+time+balance+fee);
+        }else{
+            Log.e("Hover Receiver", "no parsed variables");
+        }
+
+    }
+}
