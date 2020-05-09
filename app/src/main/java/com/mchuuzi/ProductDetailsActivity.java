4a0a682e036f4f0fa17c968828c0ebd5c445b437
Keith wachira <keithwachira@gmail.com>
2020-05-09 22:55:47 +0300

    updated prudct deatils fragment

--- app/src/main/java/com/mchuuzi/ProductDetailsActivity.java
+++ app/src/main/java/com/mchuuzi/ProductDetailsActivity.java
@@ -2,6 +2,7 @@ package com.mchuuzi;
 
 import android.os.Bundle;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
@@ -9,16 +10,24 @@ import com.mchuuzi.models.ProductsModel;
 
 public class ProductDetailsActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_details);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
         //here get the passed product
         ///clicked by use
         // fr now its hard coded
-        getSupportFragmentManager().beginTransaction().replace(R.id.frame,ProductDetailsFragment.newInstance(new ProductsModel())).commit();
+        ProductsModel product=(ProductsModel) getIntent().getSerializableExtra("product");
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setTitle(product.getName());
+        }
+        getSupportFragmentManager().beginTransaction().replace(R.id.frame,ProductDetailsFragment.newInstance(product)).commit();
 
 
 
