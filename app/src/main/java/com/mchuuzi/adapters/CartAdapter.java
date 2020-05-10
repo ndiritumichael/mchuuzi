af5e3d122efe30e144742edff8163286ed6c02a3
Keith wachira <keithwachira@gmail.com>
2020-05-10 11:06:31 +0300

    added order item fields

--- app/src/main/java/com/mchuuzi/adapters/CartAdapter.java
+++ app/src/main/java/com/mchuuzi/adapters/CartAdapter.java
@@ -4,15 +4,19 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.mchuuzi.R;
 import com.mchuuzi.models.OrderItem;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class CartAdapter  extends RecyclerView.Adapter <CartViewHolder>{
     private List<OrderItem> orderItems;
@@ -35,17 +39,30 @@ public class CartAdapter  extends RecyclerView.Adapter <CartViewHolder>{
 
     @Override
     public void onBindViewHolder(@NonNull CartViewHolder holder, int position) {
+      OrderItem orderItem=orderItems.get(position);
+      holder.quantity.setText(String.valueOf(orderItem.getQuantity()));
+      double total=orderItem.getQuantity()*orderItem.getPrice();
+
+      String amount = "KSh: " + total;
+      holder.price.setText(amount);
+      holder.product_name.setText(String.valueOf(orderItem.getName()));
+        Picasso.get().load(orderItem.getImage())
+
+                .into(holder.image);
+
 
     }
 
     @Override
     public int getItemCount() {
-        return 10;
+        return orderItems.size();
     }
 }
 
 
  class CartViewHolder extends RecyclerView.ViewHolder {
+      TextView quantity, product_name,price;
+      CircleImageView image;
 
 
     private List<OrderItem> orderItems;
@@ -53,5 +70,11 @@ public class CartAdapter  extends RecyclerView.Adapter <CartViewHolder>{
 
      CartViewHolder(@NonNull View itemView, List<OrderItem> orderItems) {
         super(itemView);
+        quantity=itemView.findViewById(R.id.quantity);
+        price=itemView.findViewById(R.id.price);
+
+        image=itemView.findViewById(R.id.image);
+        product_name=itemView.findViewById(R.id.name);
+
     }
 }
